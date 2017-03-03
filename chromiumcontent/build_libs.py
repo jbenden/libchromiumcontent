c8e8ef32f21b868edede6623adda8e89ccbd793b
Joseph Benden <joe@benden.us>
2017-03-02 19:28:16 -0700

    Switch to public defs

--- chromiumcontent/build_libs.py
+++ chromiumcontent/build_libs.py
@@ -25,7 +25,7 @@ with open(args.out, 'w') as out:
             "build",
             "chrome/browser/ui/libgtkui",
             "content",
-            "content/common",
+            "content/public/common",
             "crypto",
             "dbus",
             "device",
